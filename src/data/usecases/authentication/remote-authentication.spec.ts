47a4eed6e7cbd1298c95b3d6f2675037de94794a
Ercio Pereira <erciop@gmail.com>
2021-09-29 17:26:22 -0300

    configuração e implementação de testes

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,34 @@
+import faker from 'faker'
+import { mockAuthentication } from '../../../domain/test/mock-authentication'
+import { HttpPostClientSpy } from '../../test/mock-http-client'
+import { RemoteAuthentication } from './remote-authentication'
+
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
+describe('RemoteAuthentication', () => {
+  test('Deve chamar o HttpPostClient com a URL correta', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.auth(mockAuthentication())
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('Deve chamar o HttpPostClient com o body correto', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const authenticationParams = mockAuthentication()
+    await sut.auth(authenticationParams)
+    expect(httpPostClientSpy.body).toEqual(authenticationParams)
+  })
+})
